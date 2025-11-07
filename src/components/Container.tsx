75dc81af47e04468f3adf81539da9863a1196d42
kiiskristo <kiiskristo@gmail.com>
2025-11-06 22:22:30 -0500

    Delayed update message

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -17,7 +17,7 @@ export default function Container({ children, showHero = true }: ContainerProps)
     <div className="min-h-screen flex flex-col bg-white text-black dark:bg-gray-900 dark:text-white">
       {/* News/Updates Banner */}
       <Banner 
-        message="Perm process started again, July is the latest month! You can see daily updated cases "
+        message="Todays update will be slightly delayed check back 10:50pm EST, July is the latest month! You can see daily updated cases "
         type="info"
         dismissible={true}
         enabled={true}
